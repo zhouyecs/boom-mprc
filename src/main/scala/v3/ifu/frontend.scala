775b72b6d00bf4c7c04c0ec3ac7c1928a10bab37
ckf104 <1900011634@pku.edu.cn>
2026-01-11 17:05:39 +0800

    disable f4_btb_corrections

--- src/main/scala/v3/ifu/frontend.scala
+++ src/main/scala/v3/ifu/frontend.scala
@@ -855,7 +855,7 @@ class BoomFrontendModule(outer: BoomFrontend) extends LazyModuleImp(outer)
 
   // When f3 finds a btb mispredict, queue up a bpd correction update
   val f4_btb_corrections = Module(new Queue(new BranchPredictionUpdate, 2))
-  f4_btb_corrections.io.enq.valid := f3.io.deq.fire && f3_btb_mispredicts.reduce(_||_) && enableBTBFastRepair.B
+  f4_btb_corrections.io.enq.valid := false.B && enableBTBFastRepair.B
   f4_btb_corrections.io.enq.bits  := DontCare
   f4_btb_corrections.io.enq.bits.is_mispredict_update := false.B
   f4_btb_corrections.io.enq.bits.is_repair_update     := false.B
