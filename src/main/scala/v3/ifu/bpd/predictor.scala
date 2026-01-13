990e6acf1779013436e55840ca933282e629735e
ckf104 <1900011634@pku.edu.cn>
2026-01-13 14:30:58 +0800

    增加 f2 btb 预测 call 和 ret

--- src/main/scala/v3/ifu/bpd/predictor.scala
+++ src/main/scala/v3/ifu/bpd/predictor.scala
@@ -23,6 +23,10 @@ class BranchPrediction(implicit p: Parameters) extends BoomBundle()(p)
   val is_br           = Bool()
   // Is this a JAL?
   val is_jal          = Bool()
+  // Is this a CALL?
+  val is_call         = Bool()
+  // Is this a RET?
+  val is_ret          = Bool()
   // What is the target of his branch/jump? Do we know the target?
   val predicted_pc    = Valid(UInt(vaddrBitsExtended.W))
   // ras 的栈顶值，没有 valid 信号
@@ -71,7 +75,9 @@ class BranchPredictionUpdate(implicit p: Parameters) extends BoomBundle()(p)
   val cfi_is_jal  = Bool()
   // Was the cfi a jalr
   val cfi_is_jalr = Bool()
-  //val cfi_is_ret  = Bool()
+  // Was the cfi a call/ret?
+  val cfi_is_call      = Bool()
+  val cfi_is_ret       = Bool()
 
   val ghist = new GlobalHistory
   val lhist = Vec(nBanks, UInt(localHistoryLength.W))
@@ -105,6 +111,8 @@ class BranchPredictionBankUpdate(implicit p: Parameters) extends BoomBundle()(p)
   val cfi_is_br        = Bool()
   val cfi_is_jal       = Bool()
   val cfi_is_jalr      = Bool()
+  val cfi_is_call      = Bool()
+  val cfi_is_ret       = Bool()
 
   val ghist            = UInt(globalHistoryLength.W)
   val lhist            = UInt(localHistoryLength.W)
@@ -401,6 +409,8 @@ class BranchPredictor(implicit p: Parameters) extends BoomModule()(p)
     banked_predictors(i).io.update.bits.cfi_is_br        := io.update.bits.cfi_is_br
     banked_predictors(i).io.update.bits.cfi_is_jal       := io.update.bits.cfi_is_jal
     banked_predictors(i).io.update.bits.cfi_is_jalr      := io.update.bits.cfi_is_jalr
+    banked_predictors(i).io.update.bits.cfi_is_call      := io.update.bits.cfi_is_call
+    banked_predictors(i).io.update.bits.cfi_is_ret       := io.update.bits.cfi_is_ret
     banked_predictors(i).io.update.bits.target           := io.update.bits.target
 
     banked_lhist_providers(i).io.update.mispredict := io.update.bits.is_mispredict_update
