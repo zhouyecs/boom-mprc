29fac2d8f6cac6cee67ec405be9c4e704ee4663e
ckf104 <1900011634@pku.edu.cn>
2026-02-13 13:59:37 +0800

    fifo_pf: 增加设置 prefetch MSHR 数目的配置

--- src/main/scala/v3/common/config-mixins.scala
+++ src/main/scala/v3/common/config-mixins.scala
@@ -23,6 +23,15 @@ import boom.v3.lsu._
 // BOOM Config Fragments
 // ---------------------
 
+class PfMSHRNumber(n: Int) extends Config((site, here, up) => {
+  case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
+    case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(icache = Some(tp.tileParams.icache.get.copy(
+      pfMSHRNum = n
+    ))))
+    case other => other
+  }
+})
+
 class EnableBoomFullMSHRSkip extends Config((site, here, up) => {
   case TilesLocated(InSubsystem) => up(TilesLocated(InSubsystem), site) map {
     case tp: BoomTileAttachParams => tp.copy(tileParams = tp.tileParams.copy(core = tp.tileParams.core.copy(
